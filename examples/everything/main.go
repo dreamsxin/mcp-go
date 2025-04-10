7d815fa6afc029d28d87b5e3278ad60746554b47
zhuzongxin <dreamsxin@qq.com>
2025-04-10 07:18:21 +0100

    fix

--- examples/everything/main.go
+++ examples/everything/main.go
@@ -7,8 +7,8 @@ import (
 	"log"
 	"time"
 
-	"github.com/mark3labs/mcp-go/mcp"
-	"github.com/mark3labs/mcp-go/server"
+	"github.com/dreamsxin/mcp-go/mcp"
+	"github.com/dreamsxin/mcp-go/server"
 )
 
 type ToolName string
